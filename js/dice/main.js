b95c61a2d3617fba93929748b05dbc9fa3a3ef91
e3m5 <147027105+e3m5@users.noreply.github.com>
2025-02-18 12:18:40 +0000

    Add files via upload

---
+++ js/dice/main.js
@@ -0,0 +1,28 @@
+let start = document.getElementById ("start")
+let grafika = document.getElementById("kostka");
+let inf = document.getElementById("informacja")
+let nr = Math.random()*6+1
+nr = Math.floor(nr)
+   grafika.src = "k"+nr+".png"
+
+// function losowanie() {
+    
+    
+// }
+
+
+
+
+
+
+// function losowanie() {
+//   onclick();
+  
+//   ondblclick();
+
+//   return;
+
+//   onmouseover();
+// }
+
+// losowanie();
